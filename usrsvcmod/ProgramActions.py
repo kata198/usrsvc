0c3f5895610aff63fd2ba75b5952761db3b8bb16
Tim Savannah <kata198@gmail.com>
2016-02-29 23:13:27 -0500

    some extra comments, update license, first draft of new license addendum

--- usrsvcmod/ProgramActions.py
+++ usrsvcmod/ProgramActions.py
@@ -1,11 +1,23 @@
 '''
     Copyright (c) 2016 Tim Savannah All Rights Reserved.
-    This software is licensed under the terms of the GPLv2. This may change at my discretion.
+    This software is licensed under the terms of the GPLv3, with additions/modifications.
+    This may change at my discretion, retroactively, and without notice.
 
-    You should have received a copy of this with the source distribution as LICENSE,
+    You should have received a copy of this with the source distribution as a file titled, LICENSE,
+    and additions in a file titled LICENSE.additions. 
 
-    The most current license can be found at
+    In any situation that LICENSE.additions 
+
+    The most current license can be found at:
     https://github.com/kata198/usrsvc/LICENSE
+
+    and additions/modifications at:
+    https://github.com/kata198/usrsvc/LICENSE.additions
+
+    This location may need to be changed at some point in the future, in which case
+    you are may email Tim Savannah <kata198 at gmail dot com>, or find them on the
+    current website intended for distribution of usrsvc.
+
 
     ProgramActions - Some common actions dealing with process management.
 '''
