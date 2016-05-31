f60f561dfe9cc78e3541db4676df89487a00956e
Tim Savannah <kata198@gmail.com>
2016-05-30 22:17:50 -0400

    Add compat.py for compat stuff. Right now holds classproperty for py2

---
+++ usrsvcmod/compat.py
@@ -0,0 +1,36 @@
+'''
+    Copyright (c) 2016 Tim Savannah All Rights Reserved.
+    This software is licensed under the terms of the GPLv3.
+    This may change at my discretion, retroactively, and without notice.
+
+    You should have received a copy of this with the source distribution as a file titled, LICENSE.
+
+    The most current license can be found at:
+    https://github.com/kata198/usrsvc/LICENSE
+
+    This location may need to be changed at some point in the future, in which case
+    you are may email Tim Savannah <kata198 at gmail dot com>, or find them on the
+    current website intended for distribution of usrsvc.
+
+
+    compat stuff
+'''
+
+# vim:set ts=4 shiftwidth=4 softtabstop=4 expandtab :
+
+allList = []
+
+try:
+    classproperty
+except NameError:
+    class classproperty(object):
+        def __init__(self, getter):
+            self.getter = getter
+        def __get__(self, instance, owner):
+            return self.getter(owner)
+
+        allList.append('classproperty')
+
+__all__ = tuple(allList)
+
+# vim:set ts=4 shiftwidth=4 softtabstop=4 expandtab :
