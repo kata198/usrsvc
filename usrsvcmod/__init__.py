437b3d2897dea314f0b3dce59c7dc6c95617325e
Tim Savannah <kata198@gmail.com>
2016-06-17 12:49:20 -0400

    1.5.3

--- usrsvcmod/__init__.py
+++ usrsvcmod/__init__.py
@@ -18,7 +18,7 @@
 
 # vim:set ts=4 shiftwidth=4 softtabstop=4 expandtab :
 
-__version__ = '1.5.2'
-__version_tuple__ = (1, 5, 2)
+__version__ = '1.5.3'
+__version_tuple__ = (1, 5, 3)
 
 # vim:set ts=4 shiftwidth=4 softtabstop=4 expandtab :
