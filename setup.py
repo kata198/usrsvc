5df4ba7ee68e94cec9f1af1b75fdb3a020f4102b
Tim Savannah <kata198@gmail.com>
2016-06-23 16:41:47 -0400

    1.5.4

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
         sys.stderr.write(str(e)  +  '\n')
 
     setup(name='usrsvc',
-            version='1.5.3',
+            version='1.5.4',
             packages=['usrsvcmod', 'usrsvcmod.Monitoring', 'usrsvcmod.client'],
             scripts=['usrsvc', 'usrsvcd'],
             author='Tim Savannah',
