cc693c9c6b11be15bdf42b362149c02a586cc450
Tim Savannah <kata198@gmail.com>
2017-06-05 01:36:17 -0400

    Update systemd installer to support packages better

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 
-install -m 644 'usrsvcd@.service' /usr/lib/systemd/system
-systemctl daemon-reload
+for arg in "$@";
+do
+    if ( echo "${arg}" | grep -q '^DESTDIR=' );
+    then
+        export "${arg}"
+    fi
+done
+
+if [ -z "${DESTDIR}" ];
+then
+    DESTDIR=""
+else
+    if [ "${DESTDIR}" != "/" ];
+    then
+        DESTDIR="$(echo "${DESTDIR}" | sed 's|[/][/]*$||g')"
+        DESTDIR="$(echo "${DESTDIR}" | sed 's|//|/|g')"
+    else
+        DESTDIR=''
+    fi
+fi
+
+SYSTEMDDIR="${DESTDIR}/usr/lib/systemd/system"
+SYSTEMDDIR="$(echo "${SYSTEMDDIR}" | sed 's|//|/|g')"
+
+mkdir -p "${SYSTEMDDIR}"
+
+install -m 644 'usrsvcd@.service' "${SYSTEMDDIR}/"
+RET=$?
+if [ $RET -ne 0 ];
+then
+    echo;
+    echo;
+    echo "WARNING: Install returned non-zero. Check for errors above"'!';
+    exit ${RET}
+fi
